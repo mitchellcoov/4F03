d459fec1886f287a4eeab856fc03fbc436057aaf
BJToor <bjtoor96@gmail.com>
2018-04-24 21:57:10 -0400

    General clean up of implementation, base calculation with no other repo code used

--- nBody/main.cpp
+++ nBody/main.cpp
@@ -3,7 +3,7 @@
 
 #include <string.h>
 
-#include <omp.h>
+//#include <omp.h>
 #include <mpi.h>
 #include <math.h>
 
@@ -14,12 +14,41 @@
 #define epsilon 0.000000000000000222
 
 double G = 0.0000000000667384;
-double rangeRand(double rand, double start, double end);
+
+vec3 forceKonQ(vec3 posq, vec3 posk, double mk);
+
+struct Body{
+	//position
+	vec3 p;
+	//velocity
+	vec3 v;
+	//net acceleration
+	vec3 a;
+	//mass
+	double m;
+};
+
+//The force of particle k on particle q
+/*vec3 forceKonQ(vec3 posq, vec3 posk, double mk) {
+	vec3 diff = posq - posk;
+	double mag = diff.Magnitude;
+	return diff*(mk/mag);
+}*/
+
+/*vec3 randVelocity(double min, double max) {
+	double
+}*/
+
+//Random number in range
+double genRand(double seed, double min, double max) {
+	double r = (seed)*(max - min) + min;
+	return r;
+}
 
 int main(int argc, char* argv[]){
 
 	if( argc != 10){
-		printf("Usage: %s numParticlesLight numParticleMedium numParticleHeavy numSteps subSteps timeSubStep width height imageFilenamePrex\n", argv[0]);
+		printf("Usage: %s numParticlesLight numParticleMedium numParticleHeavy numSteps subSteps timeSubStep imageWidth imageHeight imageFilenamePrex\n", argv[0]);
 	}
 
 	MPI_Init(&argc,&argv);
@@ -31,8 +60,8 @@ int main(int argc, char* argv[]){
 
 	//variables
 	int numParticlesLight = 0;
-	int numParticleMedium = 0;
-	int numParticleHeavy = 0;
+	int numParticlesMedium = 0;
+	int numParticlesHeavy = 0;
 
 	int numSteps = 0;
 	int subSteps = 0;
@@ -44,143 +73,169 @@ int main(int argc, char* argv[]){
 
 	//root node stuff goes here
 	if(my_rank == 0){
-		int numParticlesLight = std::stoi(argv[1]);
-		int numParticlesMedium = std::stoi(argv[2]);
-		int numParticlesHeavy = std::stoi(argv[3]);
+		numParticlesLight = atoi(argv[1]);
+		numParticlesMedium = atoi(argv[2]);
+		numParticlesHeavy = atoi(argv[3]);
 
-		int numSteps = std::stoi(argv[4]);
-		int subSteps = std::stoi(argv[5]);
-		int timeSubStep = std::stoi(argv[6]);
+		numSteps = atoi(argv[4]);
+		subSteps = atoi(argv[5]);
+		timeSubStep = atoi(argv[6]);
 
-		int width = std::stoi(argv[7]);
-		int height = std::stoi(argv[8]);
+		width = atoi(argv[7]);
+		height = atoi(argv[8]);
+
+		int zDepth = 100;
+
+		Body *particles;
+		//Body *temp;
+		vec3 *accel;
+
+		image = (unsigned char *)malloc(width * height * 3 * sizeof(unsigned char));
+		//initialize image (all pixels to 0)
+		for(int i = 0; i < (width * height * 3); i++){
+			image[i] = 0;
+		}
+
+		//initialize light particles (randomized)
 		int totalParticles = numParticlesLight + numParticlesMedium + numParticlesHeavy;
+		particles = (Body *)malloc(totalParticles * sizeof(Body));
+		//temp = (Body *)malloc(totalParticles * sizeof(Body));
+		accel = (vec3 *)malloc(totalParticles * sizeof(vec3));
 
-		int * w = (int *) malloc(sizeof(int) * totalParticles);
- 		int * s_x = (int *) malloc(sizeof(int) * totalParticles);
- 		int * s_y = (int *) malloc(sizeof(int) * totalParticles);
- 		double * v_x = (double *) malloc(sizeof(double) * totalParticles);
- 		double * v_y = (double *) malloc(sizeof(double) * totalParticles);
-
-
-		image = (unsigned char *)malloc(3*width*height*sizeof(unsigned char));
-		//Create black image
-		for (int b = 0; b < (3*width*height); b++){
-			image[b] = (unsigned char) 0;
+		for(int i = 0; i < numParticlesLight; i++){
+			//mass
+			particles[i].m = genRand(drand48(), massLightMin, massLightMax);
+			//position
+			particles[i].p.x = genRand(drand48(), (double)0, (double)(width - 1));
+			particles[i].p.y = genRand(drand48(), (double)0, (double)(height - 1));
+			particles[i].p.z = genRand(drand48(), (double)0, (double)(zDepth - 1));
+			//velocity
+			if(i%2 == 0){
+				particles[i].v.x = genRand(drand48(), velocityLightMin, velocityLightMax);
+				particles[i].v.y = genRand(drand48(), velocityLightMin, velocityLightMax);
+				particles[i].v.z = genRand(drand48(), velocityLightMin, velocityLightMax);
+			}else{
+				particles[i].v.x = -1 * genRand(drand48(), velocityLightMin, velocityLightMax);
+				particles[i].v.y = -1 * genRand(drand48(), velocityLightMin, velocityLightMax);
+				particles[i].v.z = -1 * genRand(drand48(), velocityLightMin, velocityLightMax);
+			}	
 		}
-
-		//GENERATE STUFF HERE
-		#pragma omp parallel num_threads(3)
-		{
-			switch (omp_get_thread_num()) {
-				case 0:
-					for (int i = 0; i < numParticlesLight; i++) {
-							//w[i] = drand48() * (massLightMax-massLightMin+1) + massLightMin;
-							w[i] = (double) rangeRand(drand48(),massLightMin,massLightMax);
-			 				s_x[i] = drand48() * width;
-			 				s_y[i] = drand48() * height;
-
-	 				if(i%2 ==0){
-		 					v_x[i] = drand48() * (velocityLightMax-velocityLightMin+1) + velocityLightMin;
-		 					v_y[i] = drand48() * (velocityLightMax-velocityLightMin+1) + velocityLightMin;
-	 				} else{
-		 					v_x[i] = -1*(drand48() * (velocityLightMax-velocityLightMin+1) + velocityLightMin);
-		 					v_y[i] = -1*(drand48() * (velocityLightMax-velocityLightMin+1) + velocityLightMin);
-	 				}
+		//initialize medium particles (randomized)
+		for(int i = numParticlesLight; i < (numParticlesLight + numParticlesMedium); i++){
+			//mass
+			particles[i].m = genRand(drand48(), massMediumMin, massMediumMax);
+			//position
+			particles[i].p.x = genRand(drand48(), (double)0, (double)(width - 1));
+			particles[i].p.y = genRand(drand48(), (double)0, (double)(height - 1));
+			particles[i].p.z = genRand(drand48(), (double)0, (double)(zDepth - 1));
+			//velocity
+			if(i%2 == 0){
+				particles[i].v.x = genRand(drand48(), velocityMediumMin, velocityMediumMax);
+				particles[i].v.y = genRand(drand48(), velocityMediumMin, velocityMediumMax);
+				particles[i].v.z = genRand(drand48(), velocityMediumMin, velocityMediumMax);
+			}else{
+				particles[i].v.x = -1 * genRand(drand48(), velocityMediumMin, velocityMediumMax);
+				particles[i].v.y = -1 * genRand(drand48(), velocityMediumMin, velocityMediumMax);
+				particles[i].v.z = -1 * genRand(drand48(), velocityMediumMin, velocityMediumMax);
+			}
+		}
+		//initialize heavy particles (randomized)
+		for(int i = (numParticlesLight + numParticlesMedium); i < totalParticles; i++){
+			//mass
+			particles[i].m = genRand(drand48(), massHeavyMin, massHeavyMax);
+			//position
+			particles[i].p.x = genRand(drand48(), (double)0, (double)(width - 1));
+			particles[i].p.y = genRand(drand48(), (double)0, (double)(height - 1));
+			particles[i].p.z = genRand(drand48(), (double)0, (double)(zDepth - 1));
+			//velocity
+			if(i%2 == 0){
+				particles[i].v.x = genRand(drand48(), velocityHeavyMin, velocityHeavyMax);
+				particles[i].v.y = genRand(drand48(), velocityHeavyMin, velocityHeavyMax);
+				particles[i].v.z = genRand(drand48(), velocityHeavyMin, velocityHeavyMax);
+			}else{
+				particles[i].v.x = -1 * genRand(drand48(), velocityHeavyMin, velocityHeavyMax);
+				particles[i].v.y = -1 * genRand(drand48(), velocityHeavyMin, velocityHeavyMax);
+				particles[i].v.z = -1 * genRand(drand48(), velocityHeavyMin, velocityHeavyMax);
+			}
+		} 
+		//frame generation (per step)
+		for(int frame = 0; frame < numSteps; frame++){
+			//reset image pixels
+			for(int i = 0; i < (width * height * 3); i++){
+				image[i] = 0;
+			}
+			//sub step calculations
+			for(int step = 0; step < subSteps; step++){
+				//reset acceleration for particles
+				for(int i = 0; i < totalParticles; i++){
+					accel[i].x = 0;
+					accel[i].y = 0;
+					accel[i].z = 0;
 				}
-				case 1:
-					for (int i = numParticlesLight; i < numParticlesMedium + numParticlesLight; i++) {
-						//w[i] = drand48() * (massMediumMax-massMediumMin+1) + massMediumMin;
-							w[i] = (double) rangeRand(drand48(),massMediumMin,massMediumMax);
-			  			s_x[i] = drand48()*width;
-			 				s_y[i] = drand48()*height;
- 				if(i%2 ==0){
-		 					v_x[i] = drand48() * (velocityMediumMax-velocityMediumMin+1) + velocityMediumMin;
-		 					v_y[i] = drand48() * (velocityMediumMax-velocityMediumMin+1) + velocityMediumMin;
- 				} else{
-		 					v_x[i] = -1*(drand48() * (velocityMediumMax-velocityMediumMin+1) + velocityMediumMin);
-		 					v_y[i] = -1*(drand48() * (velocityMediumMax-velocityMediumMin+1) + velocityMediumMin);
- 				}
+				
+				//calculate acceleration from force of k exerted on q
+				for(int i = 0; i < totalParticles; i++){
+					Body q = particles[i];
+					for(int j = 0; j < totalParticles; j++){
+						if(i == j) continue;
+						Body k = particles[j];
+
+						vec3 dp = q.p - k.p;
+						double dist = dp.Magnitude();
+
+						double magQ = k.m / (dist * dist * dist);
+						accel[i].x -= G * magQ * dp.x;
+						accel[i].y -= G * magQ * dp.y;
+						accel[i].z -= G * magQ * dp.z;
+
+						double magK = q.m / (dist * dist * dist);
+						accel[j].x += G * magK * dp.x;
+						accel[j].y += G * magK * dp.y;
+						accel[j].z += G * magK * dp.z;
 					}
-				case 2:
-					for (int i = numParticleMedium + numParticlesLight; i < totalParticles; i++) {
-						//w[i] = drand48() * (massHeavyMax-massHeavyMin+1) + massHeavyMin;
-						w[i] = (double) rangeRand(drand48(),massHeavyMin,massHeavyMax);
-		 				s_x[i] = drand48()*width;
-		 				s_y[i] = drand48()*height;
-		 				if(i%2 ==0){
-		 					v_x[i] = drand48() * (velocityHeavyMax-velocityHeavyMin+1) + velocityHeavyMin;
-		 					v_y[i] = drand48() * (velocityHeavyMax-velocityHeavyMin+1) + velocityHeavyMin;
-		 				} else{
-		 					v_x[i] = -1*(drand48() * (velocityHeavyMax-velocityHeavyMin+1) + velocityHeavyMin);
-		 					v_y[i] = -1*(drand48() * (velocityHeavyMax-velocityHeavyMin+1) + velocityHeavyMin);
-		 				}
-					}
-			}
-		}
-		unsigned char* image = (unsigned char *) calloc(3*width*height, sizeof(unsigned char));
-
-		for(int i = 0; i < totalParticles; i++){
-
-			int index = (s_y[i] * width + s_x[i])*3;
-			if (index < (sizeof(unsigned char) *3*width*height) && index >= 0){
-				if(w[i] >= 1 && w[i] <= 5){
-					image[index] = 0;
-					image[index+1] = 0;
-					image[index+2] = 255;
-					printf("derp1 \n");
-				} else if(w[i] >= 6 && w[i] <= 10){
-					image[index] =0;
-					image[index+1] = 255;
-					image[index+2] = 0;
-					printf("derp2 \n");
-				} else if(w[i] >= 11 && w[i]<=15) {
-					image[index] = 255;
-					image[index+1] = 0;
-					image[index+2] = 0;
-					printf("derp3 \n");
 				}
-			}
-		}
+				//update position & velocity for each particle based on acceleration
+				for(int i = 0; i < totalParticles; i++){
+					particles[i].v.x += accel[i].x * timeSubStep;
+					particles[i].v.y += accel[i].y * timeSubStep;
+					particles[i].v.z += accel[i].z * timeSubStep;
 
-		for (int step = 1; step <= totalParticles, step++){
-
-			for (int particle = 0; i < totalParticles; particle++){
-
-				for (int j=i+1; j<totalParticles; j++){
-
-					forces = computeForce(s_x[i],s_y[i],w[i],s_x[j],s_y[j],w[j],0);
-
+					particles[i].p.x += particles[i].v.x * timeSubStep;
+					particles[i].p.y += particles[i].v.y * timeSubStep;
+					particles[i].p.z += particles[i].v.z * timeSubStep;
 				}
 			}
 
-		}
-
-
+			//light(red), medium(green), heavy(blue)
+			int offset;
+			for(int i = 0; i < totalParticles; i++){
+				if(i < numParticlesLight){
+					offset = (width * (int)particles[i].p.y + (int)particles[i].p.x) * 3 + 2;
+				}else if(i >= numParticlesLight && i < (numParticlesLight + numParticlesMedium)){
+					offset = (width * (int)particles[i].p.y + (int)particles[i].p.x) * 3 + 1;
+				}else{
+					offset = (width * (int)particles[i].p.y + (int)particles[i].p.x) * 3 + 0;
+				}
+				image[offset] = 255;
+			}
 
 			//almost done, just save the image
-		saveBMP(argv[9], image, width, height);
-
+			std::string filename = std::string(argv[9]) + '_';
+			std::string frameNum = std::to_string(frame);
+			filename.append(5 - frameNum.length(), '0');
+			filename = filename + frameNum + std::string(".bmp");
+			printf("%s", filename.c_str());
+			saveBMP(filename.c_str(), image, width, height);
+		}
+			
 	}
-
+	//all other nodes do this
 	else{
 
 	}
 
 	free(image);
 
-	//MPI_Finalize();
+	MPI_Finalize();
 	return 0;
-}
-//
-// //The force of particle k on particle q
-// vec3 forceKonQ(vec3 posq, vec3 posk, double mk) {
-// 	vec3 diff = posq - posk;
-// 	double mag = diff.Magnitude;
-// 	return diff*(mk/mag);
-// }
-//
-double rangeRand(double rand, double start, double end) {
-	double frand = (rand / RAND_MAX)*(end - start) + start;
-	return frand;
 }
